039214d63c484c53d7ed461b275c4ae40850c5be
krintalbogdan <krintalbogdan@gmail.com>
2025-09-26 16:00:38 -0400

    supervisor addon

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -4,7 +4,6 @@ bind = "127.0.0.1:1500"
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 0
 threads = 2
-daemon = True
 
 
 # gunicorn -w 4 -b :1500 'app:create_app()' -c gunicorn.conf.ini   
