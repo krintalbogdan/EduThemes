ee4836540c336ce9f2242107ee57f738ba44f2a8
krintalbogdan <krintalbogdan@gmail.com>
2025-09-26 15:25:17 -0400

    gunicorn configuration correction

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -2,6 +2,9 @@ import multiprocessing
 
 bind = "127.0.0.1:1500"
 workers = multiprocessing.cpu_count() * 2 + 1
-timeout = 600
+timeout = 0
+threads = 2
+daemon = True
+
 
 # gunicorn -w 4 -b :1500 'app:create_app()' -c gunicorn.conf.ini   
